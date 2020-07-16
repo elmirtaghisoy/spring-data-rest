06d45e318e7c3f20a3003d02151957cde37c131b
elmirtaghisoy <elmirtaghisoy@gmail.com>
2020-07-16 17:42:08 +0500

    added

---
+++ src/main/java/com/elmirtaghisoy/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,18 @@
+package com.elmirtaghisoy.springboot.cruddemo.dao;
+
+import com.elmirtaghisoy.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
+
+
+
+
+
+
+
+
+
+
+
